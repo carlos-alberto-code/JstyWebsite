b14927022e549b56fd7a6b2b64ee76288da93c44
Carlos Alberto <A840159943@my.uvm.edu.mx>
2025-08-31 13:16:15 -0600

    Inicio del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Home from "./components/home";
+
+function App() {
+
+  return (
+	  <div className="App">
+		  <Home title="My Home Page" username="JohnDoe" />
+	  </div>
+  )
+}
+
+export default App
